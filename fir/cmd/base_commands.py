358a95c3fd718d96698f9676f39e93e806ef02aa
Chris <weavc@pm.me>
2023-12-06 23:18:50 +0000

    Updates get_task to check for multiple matching tasks

--- fir/cmd/base_commands.py
+++ fir/cmd/base_commands.py
@@ -84,9 +84,9 @@ class CommandHandlers(CmdBuilder):
             self.context.log_task(task)
 
     def modify_task(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         if self.context.args.get("status") is not None:
             set_status = self.context.profile.set_status(task, self.context.args.get("status"))
@@ -116,9 +116,9 @@ class CommandHandlers(CmdBuilder):
             self.context.log_task(task)
 
     def remove_task(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         self.context.profile.data.tasks.remove(task)
         self.context.profile.save()
@@ -126,9 +126,9 @@ class CommandHandlers(CmdBuilder):
         return self.context.logger.log_success(f"Removed task {task.name} [{task.id}]")
 
     def task_info(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         self.context.log_task(task)
 
@@ -158,9 +158,9 @@ class CommandHandlers(CmdBuilder):
         return self.ls(self.context.args.get("status"))
 
     def add_tag(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         tags = self.context.args.get("tags")
 
@@ -174,9 +174,9 @@ class CommandHandlers(CmdBuilder):
             self.context.log_task(task)
 
     def rm_tag(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         tags = self.context.args.get("tags")
 
@@ -190,9 +190,9 @@ class CommandHandlers(CmdBuilder):
             self.context.log_task(task)
 
     def add_assigned(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         assignees = self.context.args.get("assignee")
 
@@ -206,9 +206,9 @@ class CommandHandlers(CmdBuilder):
             self.context.log_task(task)
 
     def rm_assigned(self):
-        task = self.context.profile.get_task(self.context.args.get("task_id"))
+        task, err = self.context.profile.get_task(self.context.args.get("task_id"))
         if task is None:
-            return self.context.logger.log_error("Task not found")
+            return self.context.logger.log_error(err)
 
         assignees = self.context.args.get("assignee")
 
